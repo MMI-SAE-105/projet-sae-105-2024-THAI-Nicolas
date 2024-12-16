9d7490a51ba313bce9f465c87dff8ef7923a5ea9
THAI-Nicolas <nicolas.thai@edu.univ-fcomte.fr>
2024-12-16 20:52:41 +0100

    (HTML/CSS/JS) Header, menu principale et menu langue

---
+++ src/js/script.js
@@ -0,0 +1,39 @@
+const toggle = document.querySelector(".header__end--menubtn");
+const nav = document.querySelector(".menu");
+const body = document.querySelector("body");
+const closeBtn = document.querySelector(".menu__closebtn");
+const toggleLang = document.querySelector(".header__end--langbtn");
+const navLang = document.querySelector(".lang");
+
+toggle.addEventListener("click", () => {
+    const isOpen = toggle.ariaExpanded === "true";
+    const isClosed = !isOpen;
+    console.log("isOpen : ", isOpen, "isClosed : ", isClosed);
+    nav.ariaHidden = isOpen;
+    toggle.ariaExpanded = isClosed;
+    if (isOpen) {
+        body.classList.remove("noscroll");
+    }
+    else {
+        body.classList.add("noscroll");
+    }
+})
+closeBtn.addEventListener("click", () => {
+    nav.ariaHidden = true;
+    toggle.ariaExpanded = false;
+    body.classList.remove("noscroll");
+});
+
+toggleLang.addEventListener("click", () => {
+    const isOpen = toggleLang.ariaExpanded === "true";
+    const isClosed = !isOpen;
+    console.log("isOpen : ", isOpen, "isClosed : ", isClosed);
+    navLang.ariaHidden = isOpen;
+    toggleLang.ariaExpanded = isClosed;
+    if (isOpen) {
+        bodyLang.classList.remove("noscroll");
+    }
+    else {
+        bodyLang.classList.add("noscroll");
+    }
+})
